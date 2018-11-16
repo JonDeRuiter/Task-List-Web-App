465bd2c4ad49b70a4b71fcb75731137723991987
Jon DeRuiter <jonathanhderuiter@gmail.com>
2018-11-16 14:50:22 -0500

    Don't think I'll have to nuke it now. DB connection and tables set up. Working on HTML redirects now

---
+++ TaskListWebApp/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaskListWebApp.Models
+{
+    public class Task
+    {
+        
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+
+    }
+    public class DbTask : DbContext
+    {
+        //DbSet<Task> Tasks { get; set; }
+
+        public System.Data.Entity.DbSet<TaskListWebApp.Models.Task> Tasks { get; set; }
+    }
+}
---
+++ TaskListWebApp/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaskListWebApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+
+    public class DbUser : DbContext
+    {
+        //DbSet<User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<TaskListWebApp.Models.User> Users { get; set; }
+    }
+}
